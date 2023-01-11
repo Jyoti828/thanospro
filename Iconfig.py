8a139e49726fc850e56500bfebe36e84c6ab3d20
Jyoti828 <99902970+Jyoti828@users.noreply.github.com>
2023-01-11 19:40:33 +0530

    Update Iconfig.py

--- Iconfig.py
+++ Iconfig.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 4524654
-    API_HASH = "4cfbcd31ec888cc3f0709d8d20607ac8"
+    APP_ID = 16162333
+    API_HASH = "6dbcd6b69616b03de35ea998e85f65fb"
     # the name to display in your alive message
-    ALIVE_NAME = "Շђคภ๏ร"
+    ALIVE_NAME = "Jyotipan"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "YOUR VALUE" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
+    DB_URI = "postgres://tznbqmeo:1qPkkKSpnNZ4L1wlaelx8Hs5HufPzLKt@baasu.db.elephantsql.com/tznbqmeo" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "YOUR VALUE"# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
+    STRING_SESSION = "1BVtsOGsBu3TfxxavdJu19CtdaQdNAA122E4qDOQewwjFd0eBLvbPrSG_15Eo87kvidiOxQuanE21otQZZ4x9zaUm3R71-taE2GMMAAAoOGvr-R6E2ruK7D0DsId6RTXzzy-_nViACTFZq8RCoIthqV82-uisVp28p4IVWVR2N5ftJ7yTIrH50rBQ5FtiFhzQ3sP-VabD53B5U8p1mCSjta00PeNqqJV932Ijq-hcs3Ao70Ff22vZ-gVpvzrHQeHLK4JcqzBsGq9rrSJlfyKKhWN-u-GJ80D5bE1NkhCcmM_-0nn54Je6dOA7QcIZO8IU0qaMz4V8fLUUV1NiYvWd-AhU4DYuKdc="# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
     # --------------------------------------------------
 
-    TG_BOT_TOKEN = "Your value"# create a new bot in @botfather and fill the following vales with bot token
+    TG_BOT_TOKEN = "5706352162:AAHld0AtoxEYIpR3op2TZLHLOHvD2SOxUow"# create a new bot in @botfather and fill the following vales with bot token
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001638681164
+    PRIVATE_GROUP_BOT_API_ID = -1001810897457
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
